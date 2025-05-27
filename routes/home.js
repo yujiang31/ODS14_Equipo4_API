b1d6d4b519dfc4e857681970f754ca749f1751d5
yujiang31 <yujiang.xia.7e6@itb.cat>
2025-05-27 09:40:24 +0200

    mods home

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,15 @@
 import express from "express";
+import fs from "fs";
+import bodyParser from "body-parser";
 
 
 const router = express.Router();
+
+const app = express()
+app.use(bodyParser.json());
+app.set('view engine','ejs');
+app.set('views', './views');
+
 
 router.get("/", (req, res) => {
     res.render("home");
